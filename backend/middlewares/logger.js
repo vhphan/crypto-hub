7314b7b8770b0c288c9705e5028585ad5295bfe0
VH Phan <phanveehuen@gmail.com>
2024-06-26 16:22:30 +0800

    log request response time

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -60,9 +60,20 @@ function createMyLogger(logFileName) {
     );
 }
 
+function logResponseTime(req, res, next) {
+    const startHrTime = process.hrtime();
+    res.on('finish', () => {
+        const elapsedHrTime = process.hrtime(startHrTime);
+        const elapsedTimeInMs = elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6;
+        logger.info(`${req.method} ${req.path} - ${elapsedTimeInMs/1000} s`);
+    });
+    next();
+}
+
 module.exports = {
     logger,
     logRequest,
     logError,
-    createMyLogger
+    createMyLogger,
+    logResponseTime,
 };
